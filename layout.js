4475d20866b2b955f16a9793b556f9b930706a5b
Hang0224 <keehang060224@gmail.com>
2026-01-29 14:47:15 +0800

    Add booking to navigation bar

--- layout.js
+++ layout.js
@@ -8,6 +8,7 @@ const navbarContent = `
       <li><a href="events.html">Browse Events</a></li>
       <li><a href="about.html">About Us</a></li>
       <li><a href="contact.html">Contact</a></li>
+      <li><a href="bookings.html">Booking</a></li>
       <li><a href="profile.html" class="btn-login">Profile</a></li>
     </ul>
   </nav>
